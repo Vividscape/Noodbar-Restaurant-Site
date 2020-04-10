6a241ea705aa6b3cc7dc4dd6d19109dc5d95ed18
Ehi Odigie <ehi.odigie@gmail.com>
2020-04-09 22:43:37 -0700

    Testimonial slider added

--- js/main.js
+++ js/main.js
@@ -48,3 +48,19 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+// Testimonials -------------------------------------- //
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        autoPlay:true
+    });
+});
