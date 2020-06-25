4570a52805b38f9cfa7c6c5dd1dc5c075f5e4bbf
Ehi Odigie <ehi.odigie@gmail.com>
2020-06-25 15:32:41 -0700

    configured smooth scroll navigation to account for menu height, large or small screen

--- js/main.js
+++ js/main.js
@@ -21,6 +21,27 @@ $(document).ready(function() {
       autoPlay:true
   });
 
+
+  // Smooth Scroll Menu -------------------------------------- //
+  $("#navbarNav a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+        var menu = $("nav").height();
+        console.log(menu);
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top - menu
+        }, 800);
+      } // End if
+    });
+
 });
 
 
